f6a136abbabe6fde45f06409d80217d85592b45e
Alexis Vera S. <alxvcs93@hotmail.com>
2025-07-17 21:02:11 -0500

    Instalación y uso de paquetes NuGet en proyectos .NET

---
+++ Nuget/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new Person
+        {
+            Name = "Alexis Vera",
+            Age = 32
+        };
+        string json = JsonConvert.SerializeObject(person);
+        Console.WriteLine("Serialized JSON:" + json);
+        
+        var deserializedPerson = JsonConvert.DeserializeObject<Person>(json);
+        Console.WriteLine($"Deserialized Person: Name = {deserializedPerson.Name}, Age ={deserializedPerson.Age}");
+    }
+}
